0f55511b541f12d59f3b06a1f812376866ac1e42
Basileus <Basileus@DESKTOP-M14F8KM>
2023-06-22 20:00:45 +0300

    Добавьте файлы проекта.

---
+++ SimulationOfTheCompany.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+
+class Command
+{
+    char commandType;
+    int identificator;
+};
+
+class Worker
+{
+    std::string workerName;
+    int workerID;
+    Command* command;
+
+public:
+
+    std::string getName()
+    {
+        return workerName;
+    }
+
+    int getID()
+    {
+        return workerID;
+    }
+
+    void setCommand(Command* command)
+    {
+        this->command = command;
+    }
+};
+
+class Team
+{
+    int teamID;
+    Worker* teamLider;
+    int taskCount;
+    int workerCount;
+    std::vector<Worker*> workers;
+public:
+
+    
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
